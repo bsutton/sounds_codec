c94b07520722c01e402c3ce72e567c1d1c4361db
Brett Sutton <bsutton@noojee.com.au>
2020-06-23 21:56:42 +1000

    first commit

---
+++ example/sounds_codec_example.dart
@@ -0,0 +1,6 @@
+import 'package:sounds_codec/sounds_codec.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/sounds_codec_test.dart
@@ -0,0 +1,16 @@
+import 'package:sounds_codec/sounds_codec.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    Awesome awesome;
+
+    setUp(() {
+      awesome = Awesome();
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
